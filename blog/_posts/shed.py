3ae65da9e36057e8e1f17b74a97d5bb5c688b6a2
Dominic May <me@mause.me>
2019-08-03 16:45:55 +1000

    Support other years? Maybe?

--- blog/_posts/shed.py
+++ blog/_posts/shed.py
@@ -1,11 +1,16 @@
-import json
 import requests
 from itertools import groupby as _groupby
-from datetime import datetime
-from flask import Flask, render_template
+from datetime import datetime, date, timedelta
+from flask import Flask, render_template, redirect
 
 
 app = Flask(__name__)
+
+TWO_DAYS = timedelta(days=3)
+DATES = {
+    str(date.year): (date, date + TWO_DAYS)
+    for date in (date(2018, 8, 24), date(2019, 8, 1))
+}
 
 
 def groupby(iterable, key):
@@ -17,19 +22,35 @@ def to_time(minutes: int) -> str:
 
 
 @app.route('/schedule.xml')
-def schedule_xml():
-    schedule = requests.get('https://2019.pycon-au.org/schedule/avdata.json').json()[
-        'schedule'
-    ]
+def schedule_xml(year):
+    return redirect('schedule_year_xml', year='2019')
+
+
+@app.route('/<year>/schedule.xml')
+def schedule_year_xml(year):
+    schedule = requests.get(
+        f'https://{year}.pycon-au.org/schedule/avdata.json'
+    ).json()['schedule']
+
     schedule = [
-        dict(talk, start=datetime.fromisoformat(talk['start'])) for talk in schedule
+        dict(talk, start=datetime.fromisoformat(talk['start']))
+        for talk in schedule
     ]
 
     days = groupby(schedule, lambda talk: talk['start'].date())
-    days = {date: groupby(talks, lambda talk: talk['room']) for date, talks in days}
+    days = {
+        date: groupby(talks, lambda talk: talk['room']) for date, talks in days
+    }
 
+    start_date, end_date = DATES[year]
     return (
-        render_template('schedule.xml', days=days, to_time=to_time),
+        render_template(
+            'schedule.xml',
+            days=days,
+            to_time=to_time,
+            start_date=start_date,
+            end_date=end_date,
+        ),
         200,
         {'content-type': 'application/xml'},
     )
