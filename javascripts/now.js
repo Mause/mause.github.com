a65697980b5185944b7bea4a827cd7a0f853d097
Elliana May <me@mause.me>
2026-06-01 03:42:37 +0800

    use actual lib

--- javascripts/now.js
+++ javascripts/now.js
@@ -1,14 +1,11 @@
 // @ts-check-js
 
-import { lanyard } from 'https://github.com/xaronnn/js-lanyard/raw/fdee9366ade13ab9640b250982006a5541f60664/lanyard.js'
+import { connectWebSocket } from 'https://esm.sh/lanyard-wrapper@2.0.1'
 
-
-lanyard({
-	userId: '153030101171175425',
-	socket: true,
-	onPresenceUpdate:
-		(data) => {
-			console.log('hello!', data);
-		}
-})
+connectWebSocket(
+	'153030101171175425',
+	(data) => {
+		console.log('hello!', data);
+	}
+)
 
