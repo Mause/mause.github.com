47cde5a240181187bdd68455b18a84df465304a8
Elliana May <me@mause.me>
2026-06-01 22:52:06 +0800

    spotify

--- javascripts/now.js
+++ javascripts/now.js
@@ -4,9 +4,11 @@ import { connectWebSocket } from 'https://esm.sh/lanyard-wrapper@2.0.1'
 
 connectWebSocket(
 	'153030101171175425',
-	(data) => {
-		console.log('hello!', data);
-		richStatus.textContent = `Status: ${data.discord_status}`
+	({discord_status, spotify}) => {
+		richStatus.textContent = `Status: ${discord_status}`
+		if (spotify) {
+			richStatus.textContent += ` | Currenly listening to ${spotify.song}, by ${spotify.artist}`;
+		}
 	}
 )
 
